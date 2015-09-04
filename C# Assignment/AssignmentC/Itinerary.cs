d39bda79b66bbbd2f30531c8a3c2111b67dcfeba
vivek <vrai@tavisca.com>
2015-09-04 17:27:25 +0530

    C# Assignment

---
+++ C# Assignment/AssignmentC/Itinerary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentC
+{
+    public class Itinerary
+    {
+        public string OriginAirportCode { get; set; }
+
+        public string DestinationAirportCode { get; set; }
+
+        public TimeSpan FlightTime { get; set; }
+
+        public DateTime bookingDate { get; set; }
+
+        public DateTime RoundtripDepatureDate { get; set; }
+
+        public DateTime RoundTripArrivalDate { get; set; }
+
+        public int NumberOfStops { get; set; }
+
+        public TimeSpan TotalLayoverTime { get; set; }
+
+        public string Airline { get; set; }
+
+        public DateTime UtcDepartureTime { get; set; }
+
+        public DateTime UtcArrivalTime { get; set; }
+
+        public decimal BaseFareInUSD { get; set; }
+
+        public decimal MarkupInUSD { get; set; }
+
+        public decimal TotalFareInUSD { get { return this.BaseFareInUSD + this.MarkupInUSD; } }
+    }
+
+
+
+}
---
+++ C# Assignment/AssignmentC/SearchEngine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentC
+{
+    public class SearchEngine
+    {
+        public List<Itinerary> Process(List<Itinerary> discounted)
+        {
+            List<Itinerary> SearchResults=new List<Itinerary>();
+
+            foreach (Itinerary searches in discounted)
+            {
+                searches.bookingDate = DateTime.UtcNow;
+                if (searches.OriginAirportCode == "Dallas" && (searches.RoundTripArrivalDate.Day - searches.RoundtripDepatureDate.Day < 5 && searches.RoundTripArrivalDate.Month != 12 && searches.RoundtripDepatureDate.Month != 12))
+                {
+                    SortEngine result = new SortEngine();
+                    SearchResults = result.Dallas(discounted);
+                    break;
+                }
+                else if(searches.RoundTripArrivalDate.Day - searches.RoundtripDepatureDate.Day > 5 && searches.RoundTripArrivalDate.Month == 12 && searches.RoundtripDepatureDate.Month != 12)
+                {
+                    SortEngine result = new SortEngine();
+                    SearchResults = result.HolidayPackage(discounted);
+                    break;
+                }
+
+            }
+            return SearchResults;
+        }
+    }
+}
